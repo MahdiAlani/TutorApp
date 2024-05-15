342510f2a6aed442d335da0acb569a4257e88550
Mahdi Alani <themahdi67@gmail.com>
2024-05-15 18:11:07 -0400

    Setup

---
+++ src/main/java/com/example/ututor/UTutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ututor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UTutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ututor/UTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ututor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UTutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
