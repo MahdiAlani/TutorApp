4d8cfe4556bb993ac52a7df06f0cdd59d85f5f26
Mahdi Alani <themahdi67@gmail.com>
2024-05-18 00:04:29 -0400

    user model working

---
+++ src/main/java/com/example/ututor/model/User.java
@@ -0,0 +1,43 @@
+package com.example.ututor.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String school;
+
+    public User() {}
+
+    public User(Long id, String username, String password, String email, String firstName, String lastName, String school) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.school = school;
+    }
+}
